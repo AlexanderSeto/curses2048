6d764b902a86b43dbc990bd4e71b1e61c097e321
Alexander Seto <git@seto.io>
2015-12-13 01:46:29 -0800

    Replace underscore functions with camelCase functions

--- main.c
+++ main.c
@@ -24,32 +24,32 @@ struct tile {
     int y;
 };
 
-struct game_state *init_game_state();
+struct game_state *initGameState();
 void draw(struct game_state *);
-void move_grid(struct game_state *, int);
-void add_random_tile(struct game_state *);
-void get_direction_vector(int vector[], int dir);
-void build_traversals(int traversals[2][SIZE], int *vector);
-void find_farthest_position(struct game_state *, int[4], int, int, int[2]);
-bool in_range(int);
-bool tile_matches_availible(struct game_state *);
-void init_curses();
+void moveGrid(struct game_state *, int);
+void addRandomTile(struct game_state *);
+void getDirectionVector(int vector[], int dir);
+void buildTraversals(int traversals[2][SIZE], int *vector);
+void findFarthestPosition(struct game_state *, int[4], int, int, int[2]);
+bool inRange(int);
+bool tileMatchesAvailible(struct game_state *);
+void initCurses();
 
 int main(int argc, char *argv[]) {
-    struct game_state *game = init_game_state();
+    struct game_state *game = initGameState();
     srand((unsigned)time(NULL));
 
-    add_random_tile(game);
-    add_random_tile(game);
+    addRandomTile(game);
+    addRandomTile(game);
 
-    init_curses();
+    initCurses();
 
     draw(game);
     draw(game);
 
     int c;
     while ((c = getch()) != KEY_F(1) && game->game_over != true) {
-        move_grid(game, c - 258);
+        moveGrid(game, c - 258);
         draw(game);
     }
     mvprintw(0, 10, "Sorry, you lose! Score: %d", game->total_score);
@@ -60,7 +60,7 @@ int main(int argc, char *argv[]) {
 /*
  * Initialize game state into a locally allocated game state pointer and return.
  */
-struct game_state *init_game_state() {
+struct game_state *initGameState() {
     struct game_state *game =
         (struct game_state *)malloc(sizeof(struct game_state));
     for (int i = 0; i < SIZE; i++)
@@ -75,7 +75,7 @@ struct game_state *init_game_state() {
 /*
  * Find a random location in the grid, incremenent tiles_in_play.
  */
-void add_random_tile(struct game_state *game) {
+void addRandomTile(struct game_state *game) {
     bool inserted = false;
     while (!inserted) {
         int i = rand() % SIZE;
@@ -98,7 +98,8 @@ void draw(struct game_state *game) {
         for (int j = 0; j < SIZE; j++) {
             int y, x;
             getmaxyx(stdscr, y, x);
-            local_window[i][j] = newwin( 4, 6, (y-SIZE*4)/2 + i * 4, (x-SIZE*6)/2 + j * 6);
+            local_window[i][j] = newwin(4, 6, (y - SIZE * 4) / 2 + i * 4,
+                                        (x - SIZE * 6) / 2 + j * 6);
             // I could remove box and invert colors instead.
             box(local_window[i][j], 0, 0);
             char num[4];
@@ -121,9 +122,9 @@ void move_grid(struct game_state *game, int dir) {
     mvaddch(0, 0, dir + '0');
     bool moved = false;
     int vector[2] = {0, 0};
-    get_direction_vector(vector, dir);
+    getDirectionVector(vector, dir);
     int traversals[2][SIZE];
-    build_traversals(traversals, vector);
+    buildTraversals(traversals, vector);
     for (int i = 0; i < SIZE; i++) {
         for (int j = 0; j < SIZE; j++) {
             int x = traversals[0][i];
@@ -132,20 +133,20 @@ void move_grid(struct game_state *game, int dir) {
 
             if (tile != 0) {
                 int position[4];
-                find_farthest_position(game, position, x, y, vector);
+                findFarthestPosition(game, position, x, y, vector);
                 int f_x = position[0];
                 int f_y = position[1];
                 int next_x = position[2];
                 int next_y = position[3];
                 // ONE?
-                if (in_range(next_x) && in_range(next_y) &&
+                if (inRange(next_x) && inRange(next_y) &&
                     game->grid[next_x][next_y] == game->grid[x][y]) {
                     game->grid[next_x][next_y] *= 2;
                     game->grid[x][y] = 0;
                     moved = true;
                     game->score_last_move = game->grid[next_x][next_y];
                     game->tiles_in_play--;
-                } else if (in_range(f_x) && in_range(f_y)) {
+                } else if (inRange(f_x) && inRange(f_y)) {
                     game->grid[f_x][f_y] = game->grid[x][y];
                     if (f_x != x || f_y != y) game->grid[x][y] = 0;
                     moved = true;
@@ -154,13 +155,13 @@ void move_grid(struct game_state *game, int dir) {
         }
     }
     if (moved) {
-        add_random_tile(game);
+        addRandomTile(game);
         game->total_score += game->score_last_move;
         mvprintw(0, 10, "Score %d = (+ %d)", game->total_score,
                  game->score_last_move);
 
         if ((SIZE * SIZE - game->tiles_in_play) <= 0 &&
-            !tile_matches_availible(game)) {
+            !tileMatchesAvailible(game)) {
             game->game_over = true;
         }
     }
@@ -172,7 +173,7 @@ void move_grid(struct game_state *game, int dir) {
  * 0: ( 0,  1) down         1: ( 0, -1) up
  * 2: (-1,  0) left         3: ( 1,  0) right
  */
-void get_direction_vector(int vector[2], int dir) {
+void getDirectionVector(int vector[2], int dir) {
     switch (dir) {
         case 0:  // down
             vector[0] = 1;
@@ -189,7 +190,7 @@ void get_direction_vector(int vector[2], int dir) {
     }
 }
 
-void build_traversals(int traversals[2][SIZE], int *vector) {
+void buildTraversals(int traversals[2][SIZE], int *vector) {
     for (int i = 0; i < SIZE; i++) {
         if (vector[0] == 1)
             traversals[0][i] = SIZE - 1 - i;
@@ -202,7 +203,7 @@ void build_traversals(int traversals[2][SIZE], int *vector) {
     }
 }
 
-void find_farthest_position(struct game_state *game, int position[4], int x,
+void findFarthestPosition(struct game_state *game, int position[4], int x,
                             int y, int vector[2]) {
     int prev_x, prev_y;
     do {
@@ -210,7 +211,7 @@ void find_farthest_position(struct game_state *game, int position[4], int x,
         prev_y = y;
         x = prev_x + vector[0];
         y = prev_y + vector[1];
-    } while (in_range(x) && in_range(y) && game->grid[x][y] == 0);
+    } while (inRange(x) && inRange(y) && game->grid[x][y] == 0);
 
     position[0] = prev_x;
     position[1] = prev_y;
@@ -218,17 +219,17 @@ void find_farthest_position(struct game_state *game, int position[4], int x,
     position[3] = y;
 }
 
-bool in_range(int i) { return i >= 0 && i < SIZE; }
+bool inRange(int i) { return i >= 0 && i < SIZE; }
 
-bool tile_matches_availible(struct game_state *game) {
+bool tileMatchesAvailible(struct game_state *game) {
     for (int i = 0; i < SIZE; i++) {
         for (int j = 0; j < SIZE; j++) {
             int tile = game->grid[i][j];
             if (tile) {
                 for (int dir = 0; dir < 4; dir++) {
                     int vector[2];
-                    get_direction_vector(vector, dir);
-                    if (in_range(i + vector[0]) && in_range(i + vector[1])) {
+                    getDirectionVector(vector, dir);
+                    if (inRange(i + vector[0]) && inRange(i + vector[1])) {
                         int new_tile = game->grid[i + vector[0]][j + vector[1]];
                         if (new_tile == tile) {
                             return true;
@@ -241,7 +242,7 @@ bool tile_matches_availible(struct game_state *game) {
     return false;
 }
 
-void init_curses() {
+void initCurses() {
     initscr();
     start_color();
     cbreak();
